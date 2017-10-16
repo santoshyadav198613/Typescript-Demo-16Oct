0e7a70bb2dea65e52947b4be366285cb96024c12
Santosh Yadav <santosh.yadav198613@gmail.com>
2017-10-16 08:56:28 +0530

    added a new ts solution

---
+++ datatypes.ts
@@ -0,0 +1,17 @@
+let fname: string = 'Santosh';
+let Fname: string = 'Santosh';
+
+
+let age: number = 10;
+
+let isValid: boolean = false;
+
+console.log(isValid);
+
+let dob: Date = new Date('11-nov-2013');
+console.log(dob);
+
+let unionTypes: string | number;  //union data type
+
+unionTypes = 'tersr';
+unionTypes = 10;
